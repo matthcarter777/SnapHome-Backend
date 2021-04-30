7307b39623bf54fa261fd9810dc306d031b37e8c
Mateus <mateushbsb@gmail.com>
2021-04-30 13:11:09 -0300

    middleware

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 
 import UserController  from '../controllers/UsersController';
+import EnsureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const userController = new UserController();
 
 const router = Router();
 
-router.get('/', userController.index);
+router.get('/', EnsureAuthenticated, userController.index);
 router.post('/', userController.create);
 router.get('/:id', userController.show);
 router.put('/:id', userController.update);
